8edcb3c0f702b52e4767a00e105d3320a7aee87e
talha915 <talhazafar915426@gmail.com>
2026-08-13 02:16:45 +0200

    bronze model

--- dbt-snowflake-project/dbt_snowflake/models/bronze/customers.sql
+++ dbt-snowflake-project/dbt_snowflake/models/bronze/customers.sql
@@ -8,14 +8,15 @@ SELECT
     CUSTOMER_ID,
     NAME,
     SOURCE_TIMESTAMP,
-    CURRENT_TIMESTAMP() AS LOADED_AT
+    CURRENT_TIMESTAMP() AS bronze_ingestion_time,
+    CONVERT_TIMEZONE('UTC', METADATA$ROW_LAST_COMMIT_TIME) AS updated_at 
 
 FROM {{ source('source', 'CUSTOMERS') }}
 
 {% if is_incremental() %}
 
-WHERE SOURCE_TIMESTAMP > (
-    SELECT MAX(SOURCE_TIMESTAMP)
+WHERE updated_at > (
+    SELECT MAX(updated_at)
     FROM {{ this }}
 )
 
